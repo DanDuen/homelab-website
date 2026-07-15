8ef553d16aebad5f5b66d1a39d30157813dc6bda
Daniel Duensing <duensing.daniel@gmail.com>
2026-07-15 11:53:22 -0500

    More calendar work with working dates

--- script.js
+++ script.js
@@ -17,3 +17,63 @@ function updateClock() {
 updateClock();
 
 setInterval(updateClock, 1000);
+
+
+function createCalendar() {
+
+    const calendarGrid = document.getElementById("calendar-grid");
+    const calendarHeader = document.getElementById("calendar-header");
+
+    const today = new Date();
+
+    const monthName = today.toLocaleString("default", {
+        month: "long"
+    });
+
+    const year = today.getFullYear();
+
+    calendarHeader.textContent =
+        `${monthName} ${year}`;
+
+
+    calendarGrid.innerHTML = "";
+
+    const firstDay = new Date(
+        year,
+        today.getMonth(),
+        1
+    ).getDay();
+
+    for (let i = 0; i < firstDay; i++) {
+
+        const emptyBox = document.createElement("div");
+
+        emptyBox.className = "day empty";
+
+        calendarGrid.appendChild(emptyBox);
+
+}
+
+    const daysInMonth = new Date(
+        year,
+        today.getMonth() + 1,
+        0
+    ).getDate();
+
+
+    for (let day = 1; day <= daysInMonth; day++) {
+
+        const dayBox = document.createElement("div");
+
+        dayBox.className = "day";
+
+        dayBox.innerHTML = `<strong>${day}</strong>`;
+
+        calendarGrid.appendChild(dayBox);
+
+    }
+
+}
+
+
+createCalendar();
